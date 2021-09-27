6aa118be01e2a3c2a7a296b10c4635b7212ae897
2dam <2dam@HZ375749.TartangaLH.eus>
2021-09-27 13:02:21 +0200

    Commit Jorge  Swing 27/09/2021

--- src/mvcDinReto0View/JavaFxImplementation.java
+++ src/mvcDinReto0View/JavaFxImplementation.java
@@ -5,47 +5,16 @@
  */
 package mvcDinReto0View;
 
-import javafx.application.Application;
-import javafx.event.ActionEvent;
-import javafx.event.EventHandler;
-import javafx.scene.Scene;
-import javafx.scene.control.Button;
-import javafx.scene.layout.StackPane;
-import javafx.stage.Stage;
-
 /**
  *
- * @author adril_4v37cln
+ * @author 2dam
  */
-public class JavaFxImplementation extends Application {
+public class JavaFxImplementation implements View{
+
     
     @Override
-    public void start(Stage primaryStage) {
-        Button btn = new Button();
-        btn.setText("Say 'Hello World'");
-        btn.setOnAction(new EventHandler<ActionEvent>() {
-            
-            @Override
-            public void handle(ActionEvent event) {
-                System.out.println("Hello World!");
-            }
-        });
-        
-        StackPane root = new StackPane();
-        root.getChildren().add(btn);
-        
-        Scene scene = new Scene(root, 300, 250);
-        
-        primaryStage.setTitle("Hello World!");
-        primaryStage.setScene(scene);
-        primaryStage.show();
-    }
-
-    /**
-     * @param args the command line arguments
-     */
-    public static void main(String[] args) {
-        launch(args);
+    public void showGreeting(String greeting) {
+        throw new UnsupportedOperationException("Not supported yet."); //To change body of generated methods, choose Tools | Templates.
     }
     
 }
--- src/mvcDinReto0View/JavaSwingImplementation.java
+++ src/mvcDinReto0View/JavaSwingImplementation.java
@@ -7,54 +7,19 @@ package mvcDinReto0View;
 
 /**
  *
- * @author adril_4v37cln
+ * @author 2dam
  */
-public class JavaSwingImplementation extends javax.swing.JFrame implements View{
+public class JavaSwingImplementation implements View{
 
-    /**
-     * Creates new form JavaSwingImplementation
-     */
-    public JavaSwingImplementation() {
-        initComponents();
+    JavaSwingImplementation() {
         
-        
-        
-        
-        
-        
+       
     }
-    // <editor-fold defaultstate="collapsed" desc="Generated Code">//GEN-BEGIN:initComponents
-    private void initComponents() {
-
-        setDefaultCloseOperation(javax.swing.WindowConstants.EXIT_ON_CLOSE);
-
-        javax.swing.GroupLayout layout = new javax.swing.GroupLayout(getContentPane());
-        getContentPane().setLayout(layout);
-        layout.setHorizontalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 400, Short.MAX_VALUE)
-        );
-        layout.setVerticalGroup(
-            layout.createParallelGroup(javax.swing.GroupLayout.Alignment.LEADING)
-            .addGap(0, 300, Short.MAX_VALUE)
-        );
-
-        pack();
-    }// </editor-fold>//GEN-END:initComponents
 
     @Override
     public void showGreeting(String greeting) {
-        
-        
-        
-    } 
-              
+      SwingView sv=new SwingView(greeting);
+        sv.setVisible(true);
     }
-
-    /**
-     * @param args the command line arguments
-     */
-
-    // Variables declaration - do not modify//GEN-BEGIN:variables
-    // End of variables declaration//GEN-END:variables
-
+    
+}
